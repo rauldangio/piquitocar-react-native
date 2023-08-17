14b0cb71c7d4b8679ba99ad4df52e28efb0e9e02
rauldangio <jojofagqwrt@gmail.com>
2023-08-17 08:17:20 -0300

    imagem erro

--- src/pages/Carrinho/index.tsx
+++ src/pages/Carrinho/index.tsx
@@ -1,6 +1,16 @@
-import React from "react"
-import { Text } from "react-native"
+import React from "react";
+import { Dimensions, Image, StyleSheet } from "react-native";
+import { topo } from '../../../assets/topo.png';
+
+const width = Dimensions.get('screen').width;
 
 export default function Carrinho(){
-    return <Text>Carrinho de compras</Text>
+    return <Image source={topo} style={estilos.topo}/>
 }
+
+const estilos = StyleSheet.create({
+    topo: {
+        width:"100%",
+        height: 578 / 768 * width,
+    }
+})
