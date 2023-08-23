f14b795062d85c554f4ac37eaea01540a0934918
rauldangio <raul.geraldo@etec.sp.gov.br>
2023-08-23 15:47:56 -0300

    criandos estilos

--- src/pages/Carrinho/index.tsx
+++ src/pages/Carrinho/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import {View, Dimensions, Image, StyleSheet, Text } from "react-native";
 import  topo  from "../../../assets/topo.png";
+import logo from '../../../assets/logo.png';
 
 const width = Dimensions.get('screen').width;
 
@@ -9,12 +10,15 @@ export default function Carrinho(){
     return <>
     <Image source={topo} style={estilos.topo}/>
     <Text style={estilos.titulo}>Detalhes do carrinho</Text>
-    <View>
-        <Text>Carrinho de Compras</Text>
-        <Text>Fazenda do Lorde Raul</Text>
-        <Text>Carrinho de Compras</Text>
-        <Text>Uma cesta deliciosa com produtos frescos da melhor fazenda do sudeste!</Text>
-        <Text>R$ 40,00</Text>
+    <View style={estilos.carrinho}>
+        <Text style={estilos.nome}>Carrinho de Compras</Text>
+        <View style={estilos.fazenda}>
+            <Image source={logo}/>
+            <Text style={estilos.nome}>Fazenda do Lorde Raul</Text>
+        </View>
+        <Text style={estilos.nome}>Carrinho de Compras</Text>
+        <Text style={estilos.nome}>Uma cesta deliciosa com produtos frescos da melhor fazenda do sudeste!</Text>
+        <Text style={estilos.nome}>R$ 40,00</Text>
     </View>
 
     </>
@@ -34,5 +38,20 @@ const estilos = StyleSheet.create({
         lineHeight: 26,
         padding: 16,
         fontFamily: "MontserratItalic"
+    },
+    carrinho: {
+        paddingVertical: 8,
+        paddingHorizontal: 16,
+    },
+
+    nome: {
+        fontSize: 26,
+        lineHeight: 42,
+        color: "#464646"
+    },
+
+    fazenda: {
+        flexDirection: "row",
+        paddingVertical: 12
     }
 })
