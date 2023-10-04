12ce67009dcb01d48a11d3b93eb9ae3c77fb4c4a
rauldangio <jojofagqwrt@gmail.com>
2023-10-04 15:33:51 -0300

    detalhe

--- src/pages/Carrinho/components/Detalhe.tsx
+++ src/pages/Carrinho/components/Detalhe.tsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { Text, View, Image, StyleSheet } from "react-native"
+import { Text, View, Image, StyleSheet, TouchableOpacity } from "react-native"
 import logo from "../../../../assets/logo.png"
 import TextStyle from "../../../components/Text"
 
@@ -22,6 +22,9 @@ export default function Detalhe({nome, nomeFazenda, descricao, preco, botao}: Pr
         </View>
         <Text style={style.descricao}>{descricao}</Text>
     <Text style={style.preco}>{preco}</Text>
+    <TouchableOpacity style={style.botao}>
+      <Text style={style.textobotao}>Comprar</Text>
+    </TouchableOpacity>
     </>
     )
 }
@@ -56,5 +59,18 @@ const style = StyleSheet.create({
         fontFamily: "MontSerratRegular",
         color: "#2A9F85",
         marginTop: 8
+    },
+    botao: {
+      marginTop: 16,
+      backgroundColor: "#2a9f85",
+      color: "#2a9f85",
+      borderRadius: 6
+    },
+    textobotao: {
+      textAlign: "center",
+      color: "#ffffff",
+      fontSize: 16,
+      lineHeight: 26,
+      fontFamily: "MontSerratBold"
     }
 })
